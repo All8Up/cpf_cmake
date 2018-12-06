64dbabcb939f69cdfacddb72180ae974a8284b00
All8Up <All8Up@gmail.com>
2018-12-05 22:31:00 -0600

    Downloading setup working, adding Config to download.

--- cpf.cmake
+++ cpf.cmake
@@ -1,58 +1,35 @@
 # ###############################################
-set (TOOLCHAINS_VERSION 0.0.5)
-message ("TOOLCHAINS_VERSION: ${CPF_TOOLCHAINS_VERSION}")
-
-# ###############################################
-# Setup the cache server to download prebuilt binaries.
-set(HUNTER_CACHE_SERVERS
-    "http://all8up.selfip.com:32771/artifactory/hunter"
-    CACHE
-    STRING
-    "Default cache server"
+# Setup all the configuration and versions as cache values
+# so they can be overriden as needed.
+set (CPF_TOOLCHAINS_VERSION
+    "0.0.5"
+    CACHE STRING "Toolchain release version."
+)
+set (CPF_HUNTER_GATE_FILE
+    "https://raw.githubusercontent.com/hunter-packages/gate/c4e5b3e4d1c97fb46ae86af7f59baac95f7f1cf0/cmake/HunterGate.cmake"
+    CACHE STRING "The hunter gate file to use."
+)
+set (CPF_HUNTER_CONFIG_FILE
+    ""
+    CACHE STRING "Configuration file with hunter package information."
+)
+set (CPF_CMAKE_SETUP_FILE
+    "https://raw.githubusercontent.com/All8Up/cpf_cmake/master/Setup.cmake"
+    CACHE STRING "CPF cmake setup and configuration file."
 )
 
 # ###############################################
-macro (cpf_download_release project version)
-    if (EXISTS "${CMAKE_CURRENT_LIST_DIR}/_config/${project}-v${version}.zip")
-        message ("${project} already downloaded.")
-    else ()
-        message ("Downloading ${project}.")
-        file (DOWNLOAD
-            "https://github.com/All8Up/${project}/archive/v${version}.zip"
-            "${CMAKE_CURRENT_LIST_DIR}/_config/${project}-v${version}.zip"
-        )
-        message ("Unzipping ${project}.")
-        execute_process (
-            COMMAND ${CMAKE_COMMAND} -E tar "x" "${CMAKE_CURRENT_LIST_DIR}/_config/${project}-v${version}.zip" --format=zip
-            WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/_config"
-        )
-        file (REMOVE_RECURSE "${CMAKE_CURRENT_LIST_DIR}/_config/${project}")
-        file (RENAME
-            "${CMAKE_CURRENT_LIST_DIR}/_config/${project}-${version}"
-            "${CMAKE_CURRENT_LIST_DIR}/_config/${project}"
-        )
-    endif ()
-endmacro ()
+# Dump details to console for debugging purposes.
+message ("CPF_TOOLCHAINS_VERSION: ${CPF_TOOLCHAINS_VERSION}")
+message ("CPF_HUNTER_GATE_FILE  : ${CPF_HUNTER_GATE_FILE}")
+message ("CPF_HUNTER_CONFIG_FILE: ${CPF_HUNTER_CONFIG_FILE}")
+message ("CPF_CMAKE_SETUP_FILE  : ${CPF_CMAKE_SETUP_FILE}")
 
 # ###############################################
-# If the config version is not set by the caller, download it.
+# Download the setup file and include it for further setup.
 file (DOWNLOAD
-    "https://github.com/All8Up/cpf_config/releases/download/v0.0.0/Version.txt"
-    "${CMAKE_CURRENT_LIST_DIR}/_config/Version.txt"
+    "${CPF_CMAKE_SETUP_FILE}"
+    "${CMAKE_CURRENT_LIST_DIR}/_config/Setup.cmake"
 )
 
-# ###############################################
-# Get the configuration project.
-cpf_download_release (cpf_config ${CPF_CONFIG_VERSION})
-set (CPF_CONFIG_DIR "${CMAKE_CURRENT_LIST_DIR}/_config/cpf_config")
-
-# ###############################################
-# Get the toolchains project.
-cpf_download_release (cpf_toolchains ${CPF_TOOLCHAINS_VERSION})
-
-# ###############################################
-if (NOT CPF_CONFIG)
-    set (CPF_CONFIG "${CPF_CONFIG_DIR}/Config.cmake")
-endif ()
-include ("${CPF_CONFIG_DIR}/Setup.cmake")
-include (hunter_protected_sources)
+include ("${CMAKE_CURRENT_LIST_DIR}/_config/Setup.cmake")
