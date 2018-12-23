58f8441baaf9025df2e8617456c1ec750883e43e
All8Up <All8Up@gmail.com>
2018-12-22 23:42:47 -0600

    Updates core library.

--- Config.cmake
+++ Config.cmake
@@ -1,6 +1,6 @@
 hunter_config (cpf
-    URL "https://github.com/All8Up/cpf_core/archive/v0.1.0.tar.gz"
-    SHA1 1a336b165dd4817646432ea902d258caa69cba51
+    URL "https://github.com/All8Up/cpf_core/archive/v0.1.1.tar.gz"
+    SHA1 ed383686d4d3352e09c18ea0b92ec8a2b2a78a27
     CMAKE_ARGS
         CPF_CONFIGURATION_TYPES=${CPF_CONFIGURATION_TYPES}
         CPF_TOOLCHAINS_VERSION=${CPF_TOOLCHAINS_VERSION}
